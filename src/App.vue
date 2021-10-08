d61b296ebc42ee3500be477a8d6ca0e5c7c3a56c
Joabsonlg <joabsonlg918@gmail.com>
2021-10-08 06:48:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  created () {
+    this.$q.dark.set(true)
+  }
+})
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <img src="~assets/wave.png" class="wave" alt="login-wave">
+  <div class="row" style="height: 90vh">
+    <div class="col-0 col-md-6 flex justify-center content-center">
+      <img src="~assets/login.svg" class="responsive" alt="login-image">
+    </div>
+    <div v-bind:class="{'justify-center': $q.screen.md || $q.screen.sm ||$q.screen.xs}"
+         class="col-12 col-md-6 flex content-center">
+      <q-card v-bind:style="$q.screen.lt.sm ? {'width': '80%'} : {'width': '50%'}">
+        <q-card-section>
+          <q-avatar size="103px" class="absolute-center shadow-10">
+            <img src="~assets/avatar.svg" alt="avatar">
+          </q-avatar>
+        </q-card-section>
+        <q-card-section>
+          <div class="q-pt-lg">
+            <div class="col text-h6 ellipsis flex justify-center">
+              <h2 class="text-h2 text-uppercase q-my-none text-weight-regular">Login</h2>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-form class="q-gutter-md" @submit.prevent="submitForm">
+            <q-input label="Username" v-model="login.username">
+            </q-input>
+            <q-input label="Password" type="password" v-model="login.password">
+            </q-input>
+            <div>
+              <q-btn class="full-width" color="primary" label="Login" type="submit" rounded></q-btn>
+              <div class="text-center q-mt-sm q-gutter-lg">
+                <router-link class="text-white" to="/login">Esqueceu a senha?</router-link>
+                <router-link class="text-white" to="/login">Criar conta</router-link>
+              </div>
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+let $q
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      login: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      if (!this.login.username || !this.login.password) {
+        $q.notify({
+          type: 'negative',
+          message: 'Os dados informados são inválidos.'
+        })
+      } else if (this.login.password.length < 6) {
+        $q.notify({
+          type: 'negative',
+          message: 'A senha deve ter 6 ou mais caracteres.'
+        })
+      } else {
+        console.log('login')
+      }
+    }
+  },
+  mounted () {
+    $q = useQuasar()
+  }
+}
+</script>
+
+<style scoped>
+.wave {
+  position: fixed;
+  height: 100%;
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+}
+</style>
